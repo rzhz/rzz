f6a08b33b3ed37deb976e445e0f4b600f08cbdf2
rzhz <47044990+rzhz@users.noreply.github.com>
2024-11-23 01:21:08 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,10 @@
+<script>
 function toggleAbstract(button) {
-    const abstractContent = button.nextElementSibling; // Find the next sibling (the abstract div)
-    if (abstractContent.style.display === 'block') {
+    const abstractContent = button.nextElementSibling;
+    if (abstractContent.style.display === 'block' || abstractContent.style.display === '') {
         abstractContent.style.display = 'none';
-        button.textContent = 'Show Abstract'; // Update button text
     } else {
         abstractContent.style.display = 'block';
-        button.textContent = 'Hide Abstract'; // Update button text
     }
 }
 
@@ -16,4 +15,4 @@ document.querySelectorAll(".abstract-toggle").forEach(button => {
         button.textContent = content.style.display === "block" ? "Hide Abstract" : "Show Abstract";
     });
 });
-
+<script>
