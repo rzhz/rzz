bace53ac210cd887bd4f9b7ed285a14c3f7e68af
rzhz <rzhz@users.noreply.github.com>
2024-12-01 06:02:22 +0000

    deploy: ca62c7a593c63874a004fd64886899041c2163ea

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,19 @@
 <script>
-function toggleAbstract(button) {
-    const abstractContent = button.nextElementSibling;
-    if (abstractContent.style.display === 'block' || abstractContent.style.display === '') {
-        abstractContent.style.display = 'none';
-    } else {
-        abstractContent.style.display = 'block';
-    }
-}
+        function toggleAbstract(button) {
+            const abstractContent = button.nextElementSibling;
+            if (abstractContent.style.display === 'block' || abstractContent.style.display === '') {
+                abstractContent.style.display = 'none';
+            } else {
+                abstractContent.style.display = 'block';
+            }
+        }
 
-<script>
+        document.querySelectorAll(".abstract-toggle").forEach(button => {
+            button.addEventListener("click", () => {
+                const content = button.nextElementSibling;
+                content.style.display = content.style.display === "block" ? "none" : "block";
+                button.textContent = content.style.display === "block" ? "Hide Abstract" : "Show Abstract";
+            });
+        });
+
+    </script>
